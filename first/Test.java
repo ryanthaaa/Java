b62f86b85343232153507fc4e519db2619ec3b79
ryanthaaa <friendlyrtha@gmail.com>
2026-04-09 14:56:06 +0800

    wkwk

--- first/Test.java
+++ first/Test.java
@@ -3,6 +3,7 @@ package first;
 import java.util.Scanner;
 
 public class Test {
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Siapa nama Anda? ");

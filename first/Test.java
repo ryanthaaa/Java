df0cba420ec84eda6eb0b33361dd24fabb1def18
ryanthaaa <friendlyrtha@gmail.com>
2026-04-09 14:49:16 +0800

    wlelwl

---
+++ first/Test.java
@@ -0,0 +1,18 @@
+package first;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Siapa nama Anda? ");
+        String nama = input.nextLine();
+
+        System.out.print("Berapa umur Anda? ");
+        int umur = input.nextInt();
+
+        System.out.println("Hallo " + nama + " umur kamu " + umur);
+        input.close();
+    }
+}
+

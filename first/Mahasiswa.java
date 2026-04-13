5a96b7885e3bb54d42b38aa20cc8cd17dba1895c
ryanthaaa <friendlyrtha@gmail.com>
2026-04-13 12:11:00 +0800

    wlelwel

--- first/Mahasiswa.java
+++ first/Mahasiswa.java
@@ -9,28 +9,50 @@ public class Mahasiswa {
         int umur;
         int angkatan;
         String prodi;
+        String statusUmur;
+        String statusMhs;
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Siapa nama Anda? : ");
+
+        System.out.println("<======== INPUT DATA MHS =======>");
+
+        System.out.print("Siapa nama Anda?          : ");
         nama = input.nextLine();
 
-        System.out.print("Berapa umur Anda? : ");
+        System.out.print("Berapa umur Anda?         : ");
         umur = input.nextInt();
         
-        System.out.print("Angkatan ke-berapa Anda? ");
+        System.out.print("Angkatan ke-berapa Anda?  : ");
         angkatan = input.nextInt();
 
-        System.out.print("Siapa nama Anda? : ");
+        input.nextLine();
+        System.out.print("Apa prodi Anda?           : ");
         prodi = input.nextLine();
 
 
         if (umur >= 18) {
-            System.out.println("Dewasa");
+            statusUmur = "Dewasa";
         }else {
-            System.out.println("Muda");
+            statusUmur = "Muda";
         }
 
+        if (umur > 22 && angkatan > 2025) {
+            statusMhs = "In Danger";
+        } else if (umur < 22 && angkatan <= 2025) {
+            statusMhs = "Aman";
+        } else {
+            statusMhs = "Tidak Terkateogri";
+        }
         
+        System.out.println();
+        System.out.println("<======== MAHASISWA =======>");
+        System.out.println("Nama Mahasiswa          : " + nama);
+        System.out.println("Umur Mahasiswa          : " + umur);
+        System.out.println("Angkatan Mahasiswa      : " + angkatan);
+        System.out.println("Prodi Mahasiswa         : " + prodi);
+        System.out.println("Status Umur Mahasiswa   : " + statusUmur);
+        System.out.println("Status Kuliah Mahasiswa : " + statusMhs);
+
         input.close();
     }
 }

05088205d375379ceb4bc5a7399fe1e5c68611ca
ryanthaaa <friendlyrtha@gmail.com>
2026-04-16 10:21:36 +0800

    y

---
+++ first/Main.java
@@ -0,0 +1,16 @@
+package first;
+
+public class Main {
+    public static void main(String[] args) {
+        Pasien psn1 = new Pasien();
+
+        psn1.nama = "Ryan Gosling";
+        System.out.println("Nama : " + psn1.nama);
+
+        Pasien psn2 = new Pasien();
+
+        psn2.nama = "Dika Rektor";
+        System.out.println("Nama : " + psn2.nama);
+    }
+
+}
---
+++ first/Pasien.java
@@ -0,0 +1,13 @@
+package first;
+
+public class Pasien {
+    public String noRm, nama, diagnosa;
+    public int umur;
+
+    public Pasien(String noRm, nama, diagnosa, int umur) {
+        this.noRm = noRm;
+        this.nama = nama;
+        this.diagnosa = diagnosa;
+        this.umur = umur;
+    }
+}
